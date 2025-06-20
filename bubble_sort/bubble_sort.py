ad2bc6257962fa1cc37d101c22049d51303ba951
RicAlexTM <alex.mutitu@strathmore.edu>
2025-06-20 14:29:06 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,11 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
+   
+    arr = unsorted_list[:] 
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[jThe] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr failing job is due to the bubble_sort function not being implemented. Based on the test cases in bubble_sort/test_bubble_sort.py, bubble_sort should return a sorted list, but currently, it[j]
+    return arr
     pass

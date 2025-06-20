cf1b5195952a50e61ad9da312a76a6a607fc475b
RicAlexTM <alex.mutitu@strathmore.edu>
2025-06-20 14:30:17 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,20 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
+    n = len(arr)
+    # Outer loop searching for the smallest number putting it in index i
+    for i in range(n):
+        min_index = i
+        # Inner loop searching for the smallest number from remaining list putting it in index j
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+                # Swapping the smallest value with the value at i
+                arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+# values to sort:
+my_values = [20, 18, 28, 65, 75]
+
+selection_sort(my_values)
+print("Sorted list:", my_values)
     pass
